46a9461f4c94b1e74a1eb00ba6c270d1d6d41d30
Daniel James <danieldragonhatcher@gmail.com>
2021-11-02 14:29:31 -0700

    Added luminance detection code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CameraTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CameraTest.java
@@ -3,7 +3,6 @@ package org.firstinspires.ftc.teamcode;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -20,7 +19,7 @@ public class CameraTest extends LinearOpMode {
         int cooldown = 0;
         while (opModeIsActive()) {
             if (gamepad1.a && cooldown <= 0) {
-                printTotalLumianance();
+                printTotalLuminance();
                 cooldown = 30;
             }
             if (cooldown > 0){
@@ -31,30 +30,53 @@ public class CameraTest extends LinearOpMode {
         hm.camera.closeCamera();
     }
 
-    private void printTotalLumianance() {
+    static Rectangle stone1 = new Rectangle(  0, 0, 100, 100);
+    static Rectangle stone2 = new Rectangle(100, 0, 100, 100);
+    static Rectangle stone3 = new Rectangle(200, 0, 100, 100);
+
+    private void printTotalLuminance() {
         Bitmap image = hm.camera.getLatestFrame();
 
         telemetry.clear();
         if (image != null) {
-            int width = image.getWidth();
-            int height = image.getHeight();
-
-            int count = width * height;
-            double totalL = 0;
-            for (int i = 0; i < width; i++) {
-                for (int j = 0; j < height; j++) {
-                    totalL += luminance(image.getPixel(i, j));
-                }
-            }
-            double totalLuminance = totalL / count;
-
-            telemetry.addData("average luminance", totalLuminance);
-            telemetry.update();
+            double stone1L = luminanceInRegion(image, stone1);
+            double stone2L = luminanceInRegion(image, stone2);
+            double stone3L = luminanceInRegion(image, stone3);
+            telemetry.addData("stone 1", stone1L);
+            telemetry.addData("stone 2", stone2L);
+            telemetry.addData("stone 3", stone3L);
+            String brightest = "";
+            if (stone1L > stone2L && stone1L > stone3L) brightest = "stone 1";
+            if (stone2L > stone1L && stone2L > stone3L) brightest = "stone 2";
+            if (stone3L > stone1L && stone3L > stone1L) brightest = "stone 1";
+            telemetry.addData("capstone at", brightest);
 
         } else {
             telemetry.log().add("didn't get an image");
-            telemetry.update();
         }
+        telemetry.update();
+    }
+
+    private static class Rectangle {
+        public int x, y, width, height;
+
+        Rectangle(int x, int y, int width, int height) {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+        }
+    }
+
+    private double luminanceInRegion(Bitmap image, Rectangle region) {
+        int pixels = region.width * region.height;
+        double totalL = 0;
+        for (int i = region.x; i < region.x + region.width; i++) {
+            for (int j = region.y; j < region.y + region.height; j++) {
+                totalL += luminance(image.getPixel(i, j));
+            }
+        }
+        return totalL / pixels;
     }
 
     private double luminance(int p) {
