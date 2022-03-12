986cbd18bf6874c3462e52d70ab6c456e22cce28
Daniel James <danieldragonhatcher@gmail.com>
2022-03-11 20:00:26 -0800

    Duck auto fixes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/duck_auto/DuckAutoBase.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/duck_auto/DuckAutoBase.kt
@@ -24,19 +24,18 @@ abstract class DuckAutoBase: DetectionAuto() {
 
         hw.grabberIsOpen = false
 
-        hw.turnToHeading(-45.0)
+        hw.turnToHeading(-45.0 * mult)
         hw.driveInches(15.0)
         hw.turnToHeading(90.0 * mult)
         driveFromBackDistance(0.0, hw.backDistance(), speed = 0.3)
         hw.turnToHeading(0.0)
-        driveFromBackDistance(8.0, hw.backDistance(), speed = 0.3)
+        driveFromBackDistance(if (side == Side.RED) 8.0 else 9.0, hw.backDistance(), speed = 0.1)
 
         hw.setSpinnerPower(0.6, side)
         sleep(5000)
         hw.setSpinnerPower(0.0, side)
 
         hw.turnToHeading(0.0)
-//        hw.driveInches(24.0)
         driveFromBackDistance(48.0, hw.backDistance(), speed = 0.3)
         hw.turnToHeading(90.0 * mult)
         hw.turnToHeading(90.0 * mult)
@@ -47,7 +46,8 @@ abstract class DuckAutoBase: DetectionAuto() {
         driveFromBackDistance(30.0, hw.backDistance(), speed = 0.3)
         hw.grabberIsOpen = true
 
-        hw.turnToHeading(60.0 * mult)
+        hw.driveInches(-5.0)
+        hw.turnToHeading((if (side == Side.RED) 65.0 else 60.0) * mult)
         hw.driveInches(-22.0)
         hw.turnToHeading(90.0 * mult)
         driveFromBackDistance(2.0, hw.backDistance(), speed = 0.3)
